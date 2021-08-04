c508cc4cd34bbb840d199a4faf780dfb33b223fe
Lab2M27 <Aluno@LAB2-05>
2021-08-04 16:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade1/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade1
+{
+    class Produto
+    {
+        private string Nome { get; set; }
+        private double Preco { get; set; }
+        private int Quantidade { get; set; }
+        public Produto(string Nome, double Preco, int Quantidade)
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.Quantidade = Quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void DadosAtualizados(int QuantidadeN)
+        {
+            Quantidade += QuantidadeN;
+        }
+        public void RemoverProdutos(int QuantidadeR)
+        {
+            Quantidade -= QuantidadeR;
+        }
+        public override string ToString()
+        {
+            return $"Dados atualizados do produto: {Nome}, R$ {Preco:F2}, {Quantidade} unidades, Total: R$ {ValorTotalEmEstoque():F2} ";
+        }
+    }
+}
---
+++ Atividade1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Atividade1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nome;
+            double Preco;
+            int Quantidade, QuantidadeA, QuantidadeR;
+            Console.WriteLine("Digite o nome do produto: ");
+            Nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto: ");
+            Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade em estoque do produto: ");
+            Quantidade = int.Parse(Console.ReadLine());
+            Produto produto = new Produto(Nome, Preco, Quantidade);
+            produto.ValorTotalEmEstoque();
+            Console.WriteLine(produto.ToString());
+            Console.WriteLine("Digite a quantidade de produtos a ser adicionado ao estoque: ");
+            QuantidadeA = int.Parse(Console.ReadLine());
+            produto.DadosAtualizados(QuantidadeA);
+            produto.ValorTotalEmEstoque();
+            Console.WriteLine(produto.ToString());
+            Console.WriteLine("Digite a quantidade de produtos a ser removido do estoque: ");
+            QuantidadeR = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(QuantidadeR);
+            produto.ValorTotalEmEstoque();
+            Console.WriteLine(produto.ToString());
+            Console.ReadKey();
+        }
+    }
+}
